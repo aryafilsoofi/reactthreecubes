38911578ce41ba39cffde8b4b23c1c7940cc5d39
Feelsofly <39503879+Feelsofly@users.noreply.github.com>
2022-06-15 18:32:19 -0400

    Created SpinningMesh(), lights & camera

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import './App.scss';
 import { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 
-const Box = ()=>{
+//Builds spinning shapes (args is scale)
+const SpinningMesh = ({position, args, color})=>{
     const mesh = useRef(null);
     useFrame(()=>(mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
 
     return (
-        <mesh ref={mesh}>
-            <boxBufferGeometry attach="geometry" arg={[1,1,1]}/>
-            <meshStandardMaterial attach="material"/>
+        <mesh position={position} ref={mesh}>
+            <boxBufferGeometry attach="geometry" args={args} />
+            <meshStandardMaterial attach="material" color={color}/>
         </mesh>
          )
 }
 
 function App() {
   return (
-        <Canvas>
-            <Box/>
+        <Canvas colorManagement camera={{position:[-5,2,10], fov: 60}}>
+            {/* Globally illuminate all objects in the scene equally - Does not cast shadows */}
+            <ambientLight intensity={0.3}  />
+            <SpinningMesh position={[0,1,0]} args={[3,2,1]} color="pink"/>
+            <SpinningMesh position={[-2,1,-5]} color="lightblue"/>
+            <SpinningMesh position={[5,1,-2]} color="lightblue"/>
+
         </Canvas>
   );
 }
